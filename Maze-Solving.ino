95e648783fa090ad0a6eb83f4bb727078ca5d77e
Ihsan Soydemir <i.sydmr@gmail.com>
2018-03-17 15:22:58 +0300

    Update Maze-Solving.ino

--- Maze-Solving.ino
+++ Maze-Solving.ino
@@ -1,10 +1,24 @@
-#define PWM1  6
-#define YON1  7
-#define PWM2  5                         
-#define YON2  4  
+#define SOLHIZ  6
+#define SOLM  7
+
+#define SAGHIZ  5                         
+#define SAGM  4  
+
 #define ILERI HIGH
 #define GERI LOW
-                         
+
+void ilerigit();
+
+struct Konum{
+int  x;
+int  y; 
+bool sagduvar;
+bool solduvar;
+bool onduvar;
+bool arkaduvar;
+int  referans;
+};
+
 int sensorpin =0;                 // analog pin used to connect the sharp sensor
 int onMesafe = 0;                 // variable to store the values from sensor(initially zero)
 
@@ -19,18 +33,15 @@ void setup()
 void loop() 
 { 
   int value;
-
+  int yon;
+  
   onMesafe = analogRead(sensorpin);
   int ref = 350;
  
 
   int P = 2;
   int hata = ref - onMesafe;
-
   int cosku = P*hata;
-
-
-  int yon;
 
   if(cosku <0){
     yon = GERI;
@@ -42,11 +53,11 @@ void loop()
 
   if(cosku > 255) cosku = 255;
   
-  digitalWrite(YON1, yon);
-  digitalWrite(YON2, yon);
+  digitalWrite(SOLM, yon);
+  digitalWrite(SAGM, yon);
 
-  analogWrite(PWM1, cosku);
-  analogWrite(PWM2, cosku);
+  analogWrite(SOLHIZ, cosku);
+  analogWrite(SAGHIZ, cosku);
   
 
  // sensor read 
@@ -54,4 +65,7 @@ void loop()
   Serial.println(onMesafe);            // prints the value of the sensor to the serial monitor
   delay(400);                    // wait for this much time before printing next value  
 }
+/* */
+void ilerigit(){
 
+}
